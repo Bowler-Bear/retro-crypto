79c97b71f16d5bbbd809bb76e107b26e23f5a849
Bowler-Bear <210355698+bowler-bear@users.noreply.github.com>
2025-06-14 17:58:35 -0700

    update: Prompt class defined

---
+++ src/retro/prompt.cpp
@@ -0,0 +1,58 @@
+#include "prompt.h"
+
+using namespace RetroCrypto;
+
+Prompt::Prompt()
+: Menu()
+{
+}
+
+Prompt::Prompt(std::string inTitle, std::shared_ptr<MenuTreeObject> inParent)
+: Menu(inTitle, inParent)
+{
+}
+
+
+void Prompt::drawOptions(shared_ptr<IDisplay> display, int selectedOptionIndex)
+{
+	TextBox optionBox("Continue");
+	optionBox.yPosition = MENU_BOX_Y_POSITION + MENU_TITLE_BOX_HEIGHT + PROMPT_DESCRIPTION_HEIGHT + 5;
+	optionBox.xPosition = (MENU_BOX_WIDTH-optionBox.text.size())/2;
+	optionBox.width = optionBox.text.size()+3;
+	optionBox.height = 3;
+	optionBox.setBordered();
+	optionBox.setBold();
+	optionBox.setBlinking();
+	display->drawTextBox(optionBox);
+}
+
+void Prompt::drawDescription(shared_ptr<IDisplay> display, int selectedOptionIndex)
+{
+	TextBox descriptionBox(getDescription());
+	descriptionBox.yPosition = MENU_BOX_Y_POSITION + MENU_TITLE_BOX_HEIGHT + 4;
+	descriptionBox.xPosition = MENU_BOX_X_POSITION + 2;
+	descriptionBox.width = MENU_BOX_WIDTH-3;
+	descriptionBox.height = PROMPT_DESCRIPTION_HEIGHT;
+	descriptionBox.setBordered();
+	display->drawTextBox(descriptionBox);
+}
+
+string Prompt::getDescription()
+{
+	return description;
+}
+
+void Prompt::setDescription(string newDescription)
+{
+	description = newDescription;
+}
+
+shared_ptr<MenuTreeObject> Prompt::getDestination(int selectedOptionIndex)
+{
+	return destination;
+}
+
+void Prompt::setDestination(shared_ptr<MenuTreeObject> newDestination)
+{
+	destination = newDestination;
+}
---
+++ src/retro/prompt.h
@@ -0,0 +1,28 @@
+#ifndef PROMPT_H
+#define PROMPT_H
+
+#include "menu.h"
+
+#define PROMPT_DESCRIPTION_HEIGHT 7
+
+using namespace std;
+
+namespace RetroCrypto
+{
+	class Prompt : public Menu
+	{
+	private:
+		string description;
+		shared_ptr<MenuTreeObject> destination;
+	public:
+		Prompt();
+		Prompt(string inTitle, std::shared_ptr<MenuTreeObject> inParent = nullptr);
+		virtual void drawOptions(shared_ptr<IDisplay> display, int selectedOptionIndex) override;
+		virtual void drawDescription(shared_ptr<IDisplay> display, int selectedOptionIndex) override;
+		string getDescription();
+		void setDescription(string newDescription);
+		virtual shared_ptr<MenuTreeObject> getDestination(int selectedOptionIndex) override;
+		void setDestination(shared_ptr<MenuTreeObject> newDestination);
+	};
+}
+#endif
