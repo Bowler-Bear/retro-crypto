d27d4f26294244a6adda45bf69e8d2a4a140bdaa
Bowler-Bear <210355698+bowler-bear@users.noreply.github.com>
2026-03-08 20:49:05 -0700

    update: handle multi-line descriptions for prompts

--- src/retro/prompt.cpp
+++ src/retro/prompt.cpp
@@ -28,13 +28,32 @@ void Prompt::drawOptions(shared_ptr<IDisplay> display)
 
 void Prompt::drawDescription(shared_ptr<IDisplay> display)
 {
-	TextBox descriptionBox(getDescription());
+	TextBox descriptionBox;
 	descriptionBox.yPosition = MENU_BOX_Y_POSITION + MENU_TITLE_BOX_HEIGHT + 4;
 	descriptionBox.xPosition = MENU_BOX_X_POSITION + 1;
 	descriptionBox.width = MENU_BOX_WIDTH-2;
 	descriptionBox.height = PROMPT_DESCRIPTION_HEIGHT;
 	descriptionBox.setBordered();
+	if (getDescription().length() <= descriptionBox.width)
+		descriptionBox.text = getDescription();
 	display->drawTextBox(descriptionBox);
+	if (getDescription().length() > descriptionBox.width)
+	{
+		TextBox descriptionLine;
+		descriptionLine.xPosition = descriptionBox.xPosition;
+		descriptionLine.width = descriptionBox.width;
+		descriptionLine.height = 1;
+		const uint8_t reductionOffset = 4;
+		const uint8_t lineWidth = (descriptionBox.width-reductionOffset);
+		const uint8_t descriptionLines = (getDescription().length()/lineWidth)+1;
+		descriptionLine.yPosition = MENU_BOX_Y_POSITION + MENU_TITLE_BOX_HEIGHT + 4 + ((PROMPT_DESCRIPTION_HEIGHT-2)/2+1) - (descriptionLines/2+1);
+		for(int i = 0; i < descriptionLines; i++)
+		{
+			descriptionLine.text = getDescription().substr(i*lineWidth, lineWidth);
+			descriptionLine.yPosition += 1;
+			display->drawTextBox(descriptionLine);
+		}
+	}
 }
 
 string Prompt::getDescription()
