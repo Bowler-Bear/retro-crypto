c3bc754ec6dbc083f1f710901aa7e585a216bd4e
Bowler-Bear <210355698+bowler-bear@users.noreply.github.com>
2026-05-07 22:10:13 -0700

    update: set unifont file location for cli build

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -6,6 +6,17 @@
 int main()
 {
 	RetroCrypto::CoreSystem& coreSystem = RetroCrypto::CoreSystem::getCoreSystem();
+	std::shared_ptr<UnifontHandler> unifontHandler = coreSystem.getUnifontHandler();
+	char* potentialUnifontLocations[] = { "./reduced_unifont.hex", "../reduced_unifont.hex", "../filesystem/reduced_unifont.hex" };
+	for (int i = 0 ; i < sizeof(potentialUnifontLocations)/sizeof(char*); i++)
+	{
+		if (FILE* file = fopen(potentialUnifontLocations[i], "r"))
+		{
+			fclose(file);
+			unifontHandler->setFilePath(potentialUnifontLocations[i]);
+			break;
+		}
+	}
 	std::shared_ptr<CLIDisplay> display = std::make_shared<CLIDisplay>();
 	std::shared_ptr<CLICapturer> capturer = std::make_shared<CLICapturer>();
 	coreSystem.setDisplay(display);
