9b8c57be92d7fe3e97f20fe0eef489d475f4bf32
Bowler-Bear <210355698+bowler-bear@users.noreply.github.com>
2026-08-13 18:54:43 -0700

    update: add current tick value as additional entropy to rng every 15000 vblanks for n64 build

--- src/n64/main.cpp
+++ src/n64/main.cpp
@@ -4,6 +4,24 @@
 #include "n64_display.h"
 #include "n64_capturer.h"
 
+uint32_t vBlankCounter = 0;
+
+void vBlankTriggered()
+{
+	vBlankCounter++;
+	if (vBlankCounter > N64_RESEED_EVERY_X_V_BLANKS)
+	{
+		std::shared_ptr<IRandomNumberGenerator> rng = RetroCrypto::CoreSystem::getCoreSystem().getRandomNumberGenerator();
+		const uint8_t bufferSize = sizeof(uint64_t)/sizeof(uint8_t);
+		uint8_t buffer[bufferSize] = { 0 };
+		uint64_t ticks = get_ticks();
+		for (uint8_t i = 0; i < bufferSize; i++)
+			buffer[i] = (ticks >> ((bufferSize-1-i)*8)) & 0xff;
+		rng->seedGenerator(buffer, bufferSize);
+		vBlankCounter = 0;
+	}
+}
+
 int main()
 {
 	debug_init_isviewer();
@@ -19,7 +37,7 @@ int main()
 	coreSystem.setDisplay(display);
 	coreSystem.setInputCapturer(capturer);
 	coreSystem.init();
-	register_VI_handler((void(*)(void))rand);
+	register_VI_handler((void(*)(void))vBlankTriggered);
 	while (!coreSystem.getQuitRequested())
 	{
 		coreSystem.tick();
